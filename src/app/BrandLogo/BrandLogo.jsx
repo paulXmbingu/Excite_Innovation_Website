d065e54fbe0d34b450ba0a9b4ed6f29bacd9f314
Paul <paulXmbingu@gmail.com>
2024-09-06 15:09:38 +0300

    Update

--- src/app/BrandLogo/BrandLogo.jsx
+++ src/app/BrandLogo/BrandLogo.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from './BrandLogo.module.css';
 import Logo from './Assets/Brand Logo.svg';
 
-const BrandLogoHor = () => {
+const BrandLogoHor = ({ width = '100%', height = '100%' }) => {
   return (
     <div>
       <Logo 
         className={styles.logoWrap} 
+        style={{ width, height }} 
         alt="Brand Logo"
-    />
+      />
     </div>
   );
 };
