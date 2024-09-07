4b90a6bcefab07c5702772ea9e096681c158cd87
Paul <paulXmbingu@gmail.com>
2024-09-07 23:21:16 +0300

    Update

--- src/app/Pages/Home/Home.jsx
+++ src/app/Pages/Home/Home.jsx
@@ -3,13 +3,11 @@ import styles from "./Home.modules.css"
 const HomePage = () => {
     return(
         <div className={styles.homePageWrap}>
-            {/* <div className={styles.mediaSection}>
-                <video autoPlay muted loop className={styles.video}>
-                    <source src="https://res.cloudinary.com/excit3/video/upload/v1725643817/Excite%20Company%20Website/Next-Gen_Fintech_and_Banking_Products_Showreel_2023_by_Financial_UX_Design_Agency_UXDA_hrm7d3.mp4" type="video/mp4" />
-                </video>
-            </div> */}
+            <div className={styles.mediaSection}>
+                Media Section
+            </div>
             <div className={styles.detailsSection}>
-                Details Section
+                Detail Section
             </div>
         </div>
     );
