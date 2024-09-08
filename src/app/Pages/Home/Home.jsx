b927ec13eee1e81051183e599a0ce43310399438
Paul <paulXmbingu@gmail.com>
2024-09-08 03:38:16 +0300

    Update

--- src/app/Pages/Home/Home.jsx
+++ src/app/Pages/Home/Home.jsx
@@ -1,13 +1,25 @@
-import styles from "./Home.modules.css"
+import styles from "./Home.module.css"
 
 const HomePage = () => {
     return(
         <div className={styles.homePageWrap}>
             <div className={styles.mediaSection}>
-                Media Section
+                <video 
+                    autoPlay 
+                    width="100%" 
+                    height="100%"
+                    loop
+                    className={styles.media}
+                    controls
+                >
+                    <source
+                        src="https://res.cloudinary.com/excit3/video/upload/v1725643817/Excite%20Company%20Website/Next-Gen_Fintech_and_Banking_Products_Showreel_2023_by_Financial_UX_Design_Agency_UXDA_hrm7d3.mp4"
+                        type="video/mp4"
+                    />
+                </video>
             </div>
             <div className={styles.detailsSection}>
-                Detail Section
+                Detail Sections
             </div>
         </div>
     );
