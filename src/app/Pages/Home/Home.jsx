77ab97244caa8cb232ebfb97d6cf48e208298126
Paul <paulXmbingu@gmail.com>
2024-09-08 04:10:53 +0300

    Update

--- src/app/Pages/Home/Home.jsx
+++ src/app/Pages/Home/Home.jsx
@@ -6,11 +6,10 @@ const HomePage = () => {
             <div className={styles.mediaSection}>
                 <video 
                     autoPlay 
-                    // width="100%" 
-                    height="100%"
-                    loop
-                    className={styles.media}
                     muted
+                    loop
+                    height="100%"
+                    className={styles.media}
                 >
                     <source
                         src="https://res.cloudinary.com/excit3/video/upload/v1725643817/Excite%20Company%20Website/Next-Gen_Fintech_and_Banking_Products_Showreel_2023_by_Financial_UX_Design_Agency_UXDA_hrm7d3.mp4"
