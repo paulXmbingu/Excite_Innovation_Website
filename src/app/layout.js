c96d05804bdde168e70026c117bb10095ce42fda
Paul <paulXmbingu@gmail.com>
2024-09-15 01:16:18 +0300

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,6 @@
-import localFont from "next/font/local";
+import "./globals.css"
 import Head from "next/head";
 import HomePage from "./Pages/Home/Home";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const bodyStyle = {
   margin: 0,
