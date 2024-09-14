62acef78d05c0548ea0f84b05bb5bcffee31c05f
Paul <paulXmbingu@gmail.com>
2024-09-15 01:40:28 +0300

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
         <meta property="og:image" content={metadata.openGraph.image} />
       </Head>
       <body style={bodyStyle}>
