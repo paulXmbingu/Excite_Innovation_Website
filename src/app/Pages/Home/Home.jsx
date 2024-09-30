48c7e4eeb11b60169fdc6dcb660d80d05b9e6193
Paul <paulXmbingu@gmail.com>
2024-09-30 21:42:45 +0300

    Update

--- src/app/Pages/Home/Home.jsx
+++ src/app/Pages/Home/Home.jsx
@@ -1,36 +1,6 @@
 import styles from "./Home.module.css";
 import SocialButton from '@/app/Components/Shared/Buttons/Buttons';
 
-export const metadata = {
-  title: 'Excite! Innovation Company',
-  description: 'Excite! is a premier digital product innovation company specializing in UIUX research, design, and software engineering.',
-  keywords: ['Digital Product Innovation', 'UIUX Research', 'Design', 'Software Engineering'],
-  openGraph: {
-    title: 'Excite! Innovation Company',
-    description: 'Excite! is a premier digital product innovation company specializing in UIUX research, design, and software engineering.',
-    url: 'https://excite.company/',
-    images: [
-      {
-        url: 'https://res.cloudinary.com/excit3/image/upload/v1727720945/Excite%20Company%20Website/File_Cover_kleht5.jpg',
-        alt: 'Excite! Innovation Company',
-      },
-    ],
-    siteName: 'Excite! Innovation Company',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Excite! Innovation Company',
-    description: 'Excite! is a premier digital product innovation company specializing in UIUX research, design, and software engineering.',
-    creator: '@ExciteCompany',
-    site: '@ExciteCompany',
-    images: ['https://res.cloudinary.com/excit3/image/upload/v1727720945/Excite%20Company%20Website/File_Cover_kleht5.jpg'],
-  },
-  alternates: {
-    canonical: 'https://excite.company/',
-  },
-};
-
 const HomePage = () => {
   return (
     <div className={styles.homePageWrap}>
