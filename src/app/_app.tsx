969e602a7b20a9b17de0f3a0336045413ab6414c
Paul <paulXmbingu@gmail.com>
2024-10-01 22:29:33 +0300

    Update

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* Google Tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-6X7E86H9G7"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -13,6 +14,19 @@ function MyApp({ Component, pageProps }: AppProps) {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-6X7E86H9G7');
+            `,
+          }}
+        />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PMK3LLRX');
             `,
           }}
         />
